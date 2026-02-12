dc15d1524e711728c08f6279160342783e7641b7
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2026-02-13 02:09:45 +0800

    Done with flex dimensions section

--- react-native-ui-n-interaction/index.js
+++ react-native-ui-n-interaction/index.js
@@ -3,8 +3,9 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 import LotsOfStyles from './components/Style';
 import FixedDimensionsBasics from './components/FixedDimensions';
+import FlexDimensionsBasics from './components/FlexDimensions';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(FixedDimensionsBasics);
+registerRootComponent(FlexDimensionsBasics);
